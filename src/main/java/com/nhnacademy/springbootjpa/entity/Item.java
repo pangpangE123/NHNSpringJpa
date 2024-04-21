3be3b90baaa324a8359f8dea944c669074a252c6
김민중 <minjoong.slow@gmail.com>
2024-04-21 20:23:17 +0900

    `item` 테이블에 대한 entity 매핑 실습 정답

--- src/main/java/com/nhnacademy/springbootjpa/entity/Item.java
+++ src/main/java/com/nhnacademy/springbootjpa/entity/Item.java
@@ -1,25 +1,23 @@
 package com.nhnacademy.springbootjpa.entity;
 
-// TODO #1: `item` 테이블과 매핑될 `Item` Entity 클래스를 작성하세요.
-/*
- * create table item
- * (
- *     id    bigint auto_increment
- *         primary key,
- *     name  varchar(40) not null,
- *     price bigint      not null
- * );
- */
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Entity
 public class Item {
-    public long getId() {
-        return 0;
-    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 
-    public String getName() {
-        return null;
-    }
+    @Setter
+    private String name;
 
-    public long getPrice() {
-        return 0;
-    }
+    @Setter
+    private long price;
 }
--- src/test/java/com/nhnacademy/springbootjpa/repository/ItemRepositoryTest.java
+++ src/test/java/com/nhnacademy/springbootjpa/repository/ItemRepositoryTest.java
@@ -1,7 +1,6 @@
 package com.nhnacademy.springbootjpa.repository;
 
 import com.nhnacademy.springbootjpa.entity.Item;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -9,8 +8,6 @@ import org.springframework.test.context.jdbc.Sql;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-// TODO #2: 아래 `@Disabled` 어노테이션을 삭제하고 테스트를 통과시키세요.
-@Disabled("temporary")
 @DataJpaTest
 class ItemRepositoryTest {
 
