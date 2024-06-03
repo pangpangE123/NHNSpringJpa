bb7ff7081fa2dd7be2c56012e9f7afa3e845cde7
김민중 <minjoong.slow@gmail.com>
2024-06-03 13:38:26 +0900

    JPA에서 User.age 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into "user"(id, password, age)
+values ('admin', '12345', 19),
+       ('minjoong', '67890', 22);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+create table "user"
+(
+    id       varchar(50) not null,
+    password varchar(50) not null,
+    age      int         not null,
+    primary key (id)
+);
