644af9a3446f65ff1bf65bb2c518da8841d8217c
pangpange <eric0420@naver.com>
2024-11-25 14:39:15 +0900

    commit

--- src/main/java/com/nhnacademy/springbootjpa/entity/OrderItem.java
+++ src/main/java/com/nhnacademy/springbootjpa/entity/OrderItem.java
@@ -1,5 +1,12 @@
 package com.nhnacademy.springbootjpa.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 // TODO #1: `order_item` 테이블과 매핑될 `OrderItem` Entity 클래스를 작성하세요. foreign key 부분은 무시합니다.
 /*
  * create table order_item
@@ -15,16 +22,27 @@ package com.nhnacademy.springbootjpa.entity;
  *         foreign key (order_id) references "order" (id)
  * );
  */
+@Entity
+@NoArgsConstructor
+@IdClass(OrderItemPk.class)
+@Getter
 public class OrderItem {
+
+    @Id
+    @Column(name="order_id")
+    private Long orderId;
+
+    @Id
+    @Column(name = "line_number")
+    private Integer lineNumber;
+
+    private long itemId;
+
+    private int quantity;
+
     public OrderItemPk getPk() {
-        return null;
+        return new OrderItemPk(orderId,lineNumber);
     }
 
-    public long getItemId() {
-        return 0;
-    }
 
-    public int getQuantity() {
-        return 0;
-    }
 }
--- src/main/java/com/nhnacademy/springbootjpa/entity/OrderItemPk.java
+++ src/main/java/com/nhnacademy/springbootjpa/entity/OrderItemPk.java
@@ -1,15 +1,18 @@
 package com.nhnacademy.springbootjpa.entity;
 
 // TODO #2: `OrderItem` Entity 클래스의 복합키 클래스인 `OrderItemPk` 클래스를 작성하세요.
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Getter
 public class OrderItemPk {
-    public OrderItemPk(long orderId, int lineNumber) {
-    }
+       private Long orderId;
 
-    public long getOrderId() {
-        return 0;
-    }
-
-    public int getLineNumber() {
-        return 0;
-    }
+       private Integer lineNumber;
 }
